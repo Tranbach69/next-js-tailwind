9452ac33454d394c58a8671a03f834d9c1e4019a
bach <bach.tv2000@gmail.com>
2024-04-15 22:44:33 +0700

    first commit

---
+++ src/app/Page/Home/Home.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="w-96 h-96 bg-yellowCus">
+      <div className="w-32 h-32 bg-orangeCus">
+        <div className="w-32 h-1 bg-yellowCus"></div>
+      </div>
+    </div>
+  );
+}
